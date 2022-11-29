ed43bb6b5faf79c98986a2dad29321d65e36fcdf
Raulzilla <raulzilla.dev@gmail.com>
2022-11-29 20:55:14 -0300

    feat (all): finish 1.0

--- src/generateImg.js
+++ src/generateImg.js
@@ -1,8 +1,18 @@
 import fs from "fs";
+import simpleGit from "simple-git";
 
 export const generateImg = (nameImg, img) => {
   const type = img.split("base64")[0].split("/")[1].replace(/.$/, "");
   const newImg = img.split("base64")[1].substring(1);
 
   fs.writeFileSync(`./assets/${nameImg}.${type}`, newImg, "base64");
+  publishImg();
+};
+
+const publishImg = () => {
+  const git = simpleGit.default();
+
+  git.add(".");
+  git.commit("new image");
+  git.push();
 };
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { generateImg } from "./generateImg.js";
+import { config } from "dotenv";
+
+const app = express();
+config();
+
+app.use(cors());
+
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.post("/generate-img", async (_, response) => {
+  const res = await response.req.body;
+
+  generateImg(res.nameImg, res.img);
+});
+
+app.get("/", (_, res) => {
+  res.send("API está executando normalmente");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`O servidor esta rodando na porta localhost:${process.env.PORT}`)
+);
