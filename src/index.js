b436e4a2e7e73d4c05614d1ac3e4194786e18619
Raulzilla <raulzilla.dev@gmail.com>
2022-11-29 21:27:47 -0300

    new image

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.post("/generate-img", async (_, response) => {
   const res = await response.req.body;
 
-  response.send(generateImg(res.nameImg, res.img));
+  response.status(200).json({ data: generateImg(res.nameImg, res.img) });
 });
 
 app.get("/", (_, res) => {
