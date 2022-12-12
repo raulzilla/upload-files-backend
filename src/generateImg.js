e8cb3a852c821830fc1858be7a5dd86af11c22c9
Raulzilla <raulzilla.dev@gmail.com>
2022-12-12 18:41:21 -0300

    new image

--- src/generateImg.js
+++ src/generateImg.js
@@ -4,9 +4,9 @@ import { simpleGit } from "simple-git";
 export const generateImg = (nameImg, img, type) => {
   const newImg = img.split("base64")[1].substring(1);
 
-  fs.writeFile(`./assets/${nameImg}.${type}`, newImg, "base64", (e) =>
-    console.error(e)
-  );
+  fs.writeFile(`./assets/${nameImg}.${type}`, newImg, "base64", (e) => {
+    if (e) console.error(e);
+  });
   return publishImg();
 };
 
